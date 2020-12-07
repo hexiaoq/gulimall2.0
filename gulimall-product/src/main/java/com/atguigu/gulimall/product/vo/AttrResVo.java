d6473a5121281910d7f63fd833eb87279105e1ed
hexiaoq <156437734@qq.com>
2020-12-07 16:49:30 +0800

    第一阶段的最后一次提交

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/AttrResVo.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.product.vo;
+
+import lombok.Data;
+
+@Data
+public class AttrResVo extends AttrVo {
+
+
+    private String catelogName;
+    private String groupName;
+
+    private Long[] catelogPath;
+}
